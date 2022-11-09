826a97e28fdfe1af9edf697bd5eff9ae8f983cca
Melissa Martins <113524300+MellTins@users.noreply.github.com>
2022-11-09 08:28:45 -0300

    Delete page.js

--- page.js
+++
@@ -1,8 +0,0 @@
-//Alert quando o mouse sai da pag.
-
-document.querySelector('body').onmouseleave = function(event) {
-    alert("Notamos que está indo embora :/ \nEspere um pouco! \nQuem sabe você não ganha um desconto especial.")
-    
-}
-
-//fim alert
